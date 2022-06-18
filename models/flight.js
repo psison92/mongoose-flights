74b7817e2192969fb2903e93e1ebb1228ab1796a
psison <sisonpatrick29@yahoo.com>
2022-06-18 10:55:51 -0500

    compile flight schema into model

---
+++ models/flight.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+    airline: String,
+    airport: String,
+    flightNo: Number,
+    departs: Date
+}, {
+    timestamps: true
+})
+
+// Compile the schema into a model and export it
+const Flight = mongoose.model('Flight', flightSchema)
+
+export {
+    Flight
+}
